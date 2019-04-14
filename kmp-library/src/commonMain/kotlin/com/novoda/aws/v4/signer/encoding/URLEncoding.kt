dbf879ebb2facd97785a01f7e2317830a1426bfd
Tobias Heine <t.heine84@gmail.com>
2019-04-14 20:03:45 +0200

    Just remove utf-8 encoding tests

--- kmp-library/src/commonMain/kotlin/com/novoda/aws/v4/signer/encoding/URLEncoding.kt
+++ kmp-library/src/commonMain/kotlin/com/novoda/aws/v4/signer/encoding/URLEncoding.kt
@@ -1,8 +1,8 @@
 package com.novoda.aws.v4.signer.encoding
 
+import io.ktor.http.encodeURLParameter
 import io.ktor.http.encodeURLPath
-import io.ktor.http.encodeURLQueryComponent
 
 fun encodePath(path: String): String = path.encodeURLPath()
 
-fun encodeQueryComponent(value: String): String = value.encodeURLQueryComponent()
+fun encodeQueryComponent(value: String): String = value.encodeURLParameter()
