6634c5e4f863bd1e14fd38a84a264702a795ea7e
Tobias Heine <tobias@novoda.com>
2019-04-16 09:58:35 +0200

    Add ios sample

---
+++ ios/Sample/Sample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by Tobias Heine on 16.04.19.
+//  Copyright © 2019 Novoda. All rights reserved.
+//
+
+import UIKit
+import main
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
