18867cb86025e63afe1434668872793755940d57
Tobias Heine <tobias@novoda.com>
2019-04-17 10:24:14 +0200

    Implement native hmac encoder

--- kmp-library/src/iosMain/kotlin/com/novoda/aws/v4/signer/hash/Hmac256Encoder.kt
+++ kmp-library/src/iosMain/kotlin/com/novoda/aws/v4/signer/hash/Hmac256Encoder.kt
@@ -1,7 +1,26 @@
 package com.novoda.aws.v4.signer.hash
 
+import kotlinx.cinterop.*
+import platform.CoreCrypto.CCHmac
+import platform.CoreCrypto.CC_SHA256_DIGEST_LENGTH
+import platform.CoreCrypto.kCCHmacAlgSHA256
+import platform.Foundation.*
+
 actual object Hmac256Encoder {
+    @ExperimentalUnsignedTypes
     actual fun encode(key: ByteArray, value: String): ByteArray {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        memScoped {
+            val dataIn = (value as NSString).dataUsingEncoding(NSUTF8StringEncoding.toULong())!!
+            val macOut = NSMutableData.dataWithLength(CC_SHA256_DIGEST_LENGTH.convert())!!
+            val keyRef = key as CValuesRef<ByteVar>
+            val keyLength = key.size.toULong()
+
+            CCHmac(kCCHmacAlgSHA256, keyRef, keyLength, (dataIn.bytes as CValuesRef<ByteVar>), dataIn.length, macOut.mutableBytes as CValuesRef<UByteVar>)
+
+            val ba = macOut.bytes!!
+            val len = macOut.length.toInt()
+
+            return ba.readBytes(len)
+        }
     }
 }
