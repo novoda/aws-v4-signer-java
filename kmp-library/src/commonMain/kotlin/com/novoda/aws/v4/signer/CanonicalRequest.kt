da1aad567391d23fb2e72d485bd1f55a968c3d31
Jozef Celuch <jozef@novoda.com>
2019-05-13 10:06:36 +0200

    Remove license information

--- kmp-library/src/commonMain/kotlin/com/novoda/aws/v4/signer/CanonicalRequest.kt
+++ kmp-library/src/commonMain/kotlin/com/novoda/aws/v4/signer/CanonicalRequest.kt
@@ -1,15 +1,3 @@
-/*
-  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
-  the License. You may obtain a copy of the License at
-
-  http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
-  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
-  specific language governing permissions and limitations under the License.
-
-  Copyright 2016 the original author or authors.
- */
 package com.novoda.aws.v4.signer
 
 import com.novoda.aws.v4.signer.encoding.URLEncoding
@@ -18,9 +6,6 @@ private const val S3_SERVICE = "s3"
 private const val QUERY_PARAMETER_SEPARATOR = '&'
 private const val QUERY_PARAMETER_VALUE_SEPARATOR = '='
 
-/**
- * @author Richard Lucas
- */
 class CanonicalRequest(
         private val service: String,
         private val httpRequest: HttpRequest,
