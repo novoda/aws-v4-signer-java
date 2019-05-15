162066bea52d121ee9a4d0b1a5cb9f1e287e3433
Tobias Heine <tobias@novoda.com>
2019-05-15 18:03:39 +0200

    Remove license information

--- kmp-library/src/commonMain/kotlin/com/novoda/aws/v4/signer/Signer.kt
+++ kmp-library/src/commonMain/kotlin/com/novoda/aws/v4/signer/Signer.kt
@@ -1,15 +1,3 @@
-/*
-  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
-  the License. You may obtain a copy of the License at
-
-  http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
-  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
-  specific language governing permissions and limitations under the License.
-
-  Copyright 2016 the original author or authors.
- */
 package com.novoda.aws.v4.signer
 
 
@@ -20,9 +8,6 @@ import com.novoda.aws.v4.signer.hash.toHexString
 import com.novoda.aws.v4.signer.hash.toUtf8ByteArray
 import kotlin.jvm.JvmStatic
 
-/**
- * @author Richard Lucas
- */
 class Signer private constructor(private val request: CanonicalRequest, private val awsCredentials: AwsCredentials, private val date: String, private val scope: CredentialScope) {
 
     val canonicalRequest: String
