a2f6da2a061ce2d7645f673335ff9713137056f1
Tobias Heine <tobias@novoda.com>
2019-05-27 15:22:50 +0200

    Add example of how to create a signature

--- samples/ios/Sample/ViewController.swift
+++ samples/ios/Sample/ViewController.swift
@@ -5,7 +5,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let hash = Sha256Encoder().encode(value: "content as string")
+        let request = HttpRequest.Companion().create(method: "PUT", pathAndQuery: "/-/vaults/examplevault")
+        let signature = Signer.Builder()
+            .awsCredentials(awsCredentials: AwsCredentials(accessKey: "AKIAIOSFODNN7EXAMPLE", secretKey: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"))
+            .header(header: Header(name: "Host", value: "glacier.us-east-1.amazonaws.com"))
+            .header(header: Header(name: "x-amz-date", value: "20120525T002453Z"))
+            .header(header: Header(name: "x-amz-glacier-version", value: "2012-06-01"))
+            .buildGlacier(request: request, contentSha256: hash)
+            .signature
+        
+        print(signature)
     }
 
     override func didReceiveMemoryWarning() {
@@ -13,6 +24,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
